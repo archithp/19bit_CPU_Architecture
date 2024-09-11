47eaf3cf709595481afdff1d794dc6303ca02ae4
archithp <archith100@gmail.com>
2024-09-11 11:52:28 +0530

    created ALU control module

---
+++ alu_control.v
@@ -0,0 +1,18 @@
+`include "parameter.v"
+
+module alu_control (
+    input [4:0] opcode,
+    input [4:0] funct,
+    output reg [4:0] operation
+);
+    
+    always @(*) begin
+        case (opcode)
+            `ART: operation = funct;
+            `LOG: operation = funct; 
+            default: operation = 5'd0;
+        endcase
+    end
+
+
+endmodule
