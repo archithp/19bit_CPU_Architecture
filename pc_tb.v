3cb65da3df9a3e8461a7408117b85611747e2e59
archithp <archith100@gmail.com>
2024-09-12 20:24:57 +0530

    added reset signal, removed clk dependency

--- pc_tb.v
+++ pc_tb.v
@@ -4,10 +4,12 @@ reg [7:0] next_pc;
 wire [7:0] pc;
 reg clk,pcwrite;
 
-pc pc_cpu(  .clk(clk),
-            .pcwrite(pcwrite),
-            .next_pc(next_pc),
-            .pc(pc));
+pc pc_cpu(  
+    .clk(clk),
+    .reset(reset),
+    .pcwrite(pcwrite),
+    .next_pc(next_pc),
+    .pc(pc));
 
 initial begin
     clk = 0;
@@ -20,13 +22,13 @@ end
 initial begin
     $dumpfile("waveform.vcd");
     $dumpvars(0,pc_tb);
-    #1 next_pc = 8'd0; pcwrite = 1'b0;
-    #1 next_pc = 8'd1; pcwrite = 1'b0;
-    #1 next_pc = 8'd2; pcwrite = 1'b0;
-    #1 next_pc = 8'd3; pcwrite = 1'b1;
-    #1 next_pc = 8'd4; pcwrite = 1'b1;
-    #1 next_pc = 8'd3; pcwrite = 1'b1;
-    #1 next_pc = 8'd4; pcwrite = 1'b0;
+    #1 next_pc = 8'd0; pcwrite = 1'b0; reset = 1'b0;
+    #1 next_pc = 8'd1; pcwrite = 1'b0; reset = 1'b0;
+    #1 next_pc = 8'd2; pcwrite = 1'b0; reset = 1'b0;
+    #1 next_pc = 8'd3; pcwrite = 1'b1; reset = 1'b0;
+    #1 next_pc = 8'd4; pcwrite = 1'b1; reset = 1'b0;
+    #1 next_pc = 8'd3; pcwrite = 1'b1; reset = 1'b0;
+    #1 next_pc = 8'd4; pcwrite = 1'b0; reset = 1'b0;
 
     #1 $finish;
 
