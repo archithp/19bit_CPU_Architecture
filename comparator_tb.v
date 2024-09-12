99ebdc6d94bb7d22771a3b0362574d30755086db
archithp <archith100@gmail.com>
2024-09-12 19:50:53 +0530

    variable name change

--- comparator_tb.v
+++ comparator_tb.v
@@ -1,17 +1,19 @@
 module comparator_tb;
-    reg [31:0] rd1,rd2;
+    reg [31:0] ID_rd1,ID_rd2;
     wire zero;
 
-    comparator comparator_cpu(.rd1(rd1),.rd2(rd2),.zero(zero));
+    comparator comparator_cpu(  .ID_rd1(ID_rd1),
+                                .ID_rd2(ID_rd2),
+                                .zero(zero));
 
     initial begin
         $dumpfile("waveform.vcd");
         $dumpvars(0,comparator_tb);
 
-        #1 rd1 = 32'd0; rd2 = 32'd0;
-        #1 rd1 = 32'd1; rd2 = 32'd0;
-        #1 rd1 = 32'd0; rd2 = 32'd2; 
-        #1 rd1 = 32'd4; rd2 = 32'd4;
+        #1 ID_rd1 = 32'd0; ID_rd2 = 32'd0;
+        #1 ID_rd1 = 32'd1; ID_rd2 = 32'd0;
+        #1 ID_rd1 = 32'd0; ID_rd2 = 32'd2; 
+        #1 ID_rd1 = 32'd4; ID_rd2 = 32'd4;
         #1 $finish;
 
     end
