bded22d49bdd011646fe8127ae1124bdae84b8a8
archithp <archith100@gmail.com>
2024-09-12 20:20:27 +0530

    added opcode in port

--- ID_EX_tb.v
+++ ID_EX_tb.v
@@ -4,13 +4,16 @@ reg ID_regwrite, ID_memtoreg, ID_memread, ID_memwrite, ID_alusrc, ID_aluop, ID_r
 reg [7:0] ID_immediate;
 reg [2:0] ID_rs, ID_rt, ID_rd;
 reg [31:0] ID_rd1, ID_rd2;
+reg [5:0] ID_opcode;
 
 wire EX_regwrite, EX_memtoreg, EX_memread, EX_memwrite, EX_alusrc, EX_aluop, EX_regdist;
 wire [7:0] EX_immediate;
 wire [2:0] EX_rs, EX_rt, EX_rd;
 wire [31:0] EX_rd1, EX_rd2;
+wire [5:0] EX_opcode;
 
 ID_EX ID_EX_cpu(.clk(clk),
+                .ID_opcode(ID_opcode),
                 .ID_regwrite(ID_regwrite),
                 .ID_memtoreg(ID_memtoreg),
                 .ID_memread(ID_memread),
@@ -24,6 +27,7 @@ ID_EX ID_EX_cpu(.clk(clk),
                 .ID_rd(ID_rd),
                 .ID_rd1(ID_rd1),
                 .ID_rd2(ID_rd2),
+                .EX_opcode(EX_opcode),
                 .EX_regwrite(EX_regwrite),
                 .EX_memtoreg(EX_memtoreg),
                 .EX_memread(EX_memread),
@@ -47,6 +51,7 @@ initial begin
     $dumpfile("waveform.vcd");
     $dumpvars(0,ID_EX_tb);
     #1 
+    ID_opcode = 5'd2;
     ID_regwrite = 1'b0; 
     ID_memtoreg = 1'b0;
     ID_memread = 1'b0;
@@ -62,6 +67,7 @@ initial begin
     ID_rd2 = 32'd43;
 
     #2 
+    ID_opcode = 5'd8;
     ID_regwrite = 1'b1; 
     ID_memtoreg = 1'b1;
     ID_memread = 1'b1;
