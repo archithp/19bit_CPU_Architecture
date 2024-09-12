1449c1fa7030273db9a49426a6a4255f2a2f31ae
archithp <archith100@gmail.com>
2024-09-12 20:23:15 +0530

    rename ports

--- IF_ID_tb.v
+++ IF_ID_tb.v
@@ -1,17 +1,17 @@
 module IF_ID_tb;
     reg clk,IF_IDwrite;
-    reg [7:0] pc_plus_one_IF;
-    wire [7:0] pc_plus_one_ID;
-    reg [18:0] next_instruction;
-    wire [18:0] instruction;
+    reg [7:0] IF_pc_plus_one;
+    wire [7:0] ID_pc_plus_one;
+    reg [18:0] IF_instruction;
+    wire [18:0] ID_instruction;
 
 
 IF_ID IF_ID_cpu (   .clk(clk),
-                    .next_instruction(next_instruction),
+                    .IF_instruction(IF_instruction),
                     .IF_IDwrite(IF_IDwrite),
-                    .pc_plus_one_IF(pc_plus_one_IF),
-                    .pc_plus_one_ID(pc_plus_one_ID),
-                    .instruction(instruction));
+                    .IF_pc_plus_one(IF_pc_plus_one),
+                    .ID_pc_plus_one(ID_pc_plus_one),
+                    .ID_instruction(ID_instruction));
 initial begin
     clk = 0;
     forever #1 clk = ~clk;
@@ -22,12 +22,12 @@ initial begin
 
     $dumpfile("waveform.vcd");
     $dumpvars(0,IF_ID_tb);
-    #1 next_instruction = 19'd0; IF_IDwrite = 1'b1; pc_plus_one_IF = 8'd0;
-    #1 next_instruction = 19'd1; IF_IDwrite = 1'b1; pc_plus_one_IF = 8'd1;
-    #1 next_instruction = 19'd2; IF_IDwrite = 1'b1; pc_plus_one_IF = 8'd2;
-    #1 next_instruction = 19'd3; IF_IDwrite = 1'b0; pc_plus_one_IF = 8'd3;
-    #1 next_instruction = 19'd4; IF_IDwrite = 1'b0; pc_plus_one_IF = 8'd4;
-    #1 next_instruction = 19'd5; IF_IDwrite = 1'b1; pc_plus_one_IF = 8'd5;
+    #1 IF_instruction = 19'd0; IF_IDwrite = 1'b1; IF_pc_plus_one = 8'd0;
+    #1 IF_instruction = 19'd1; IF_IDwrite = 1'b1; IF_pc_plus_one = 8'd1;
+    #1 IF_instruction = 19'd2; IF_IDwrite = 1'b1; IF_pc_plus_one = 8'd2;
+    #1 IF_instruction = 19'd3; IF_IDwrite = 1'b0; IF_pc_plus_one = 8'd3;
+    #1 IF_instruction = 19'd4; IF_IDwrite = 1'b0; IF_pc_plus_one = 8'd4;
+    #1 IF_instruction = 19'd5; IF_IDwrite = 1'b1; IF_pc_plus_one = 8'd5;
 
     #1 $finish;
 
