609b92e1beb4573e54000e1f9f473279baab7e1f
archithp <archith100@gmail.com>
2024-09-12 19:49:38 +0530

    variable name change

--- alu.v
+++ alu.v
@@ -4,23 +4,23 @@ module alu (
     input [31:0] in1,
     input [31:0] in2,
     input [4:0] alu_ctrl,
-    output reg [31:0] out
+    output reg [31:0] EX_out
 );
     
     always @(*) begin
         case (alu_ctrl)
-            `ADD: out = in1 + in2;
-            `SUB: out = in1 - in2;
-            `MUL: out = in1 * in2;
-            `DIV: out = in1 / in2;
-            `INC: out = in1 + 1;
-            `DEC: out = in1 - 1;
-            `AND: out = in1 & in2;
-            `OR: out = in1 | in2;
-            `XOR: out = in1 ^ in2;
-            `ENC: out = in1 ^ 32'b00110010101110101100100000011001; //XOR Encryption 
-            `DECRY: out = in1 ^ 32'b00110010101110101100100000011001;
-            default: out = 32'd0;
+            `ADD: EX_out = in1 + in2;
+            `SUB: EX_out = in1 - in2;
+            `MUL: EX_out = in1 * in2;
+            `DIV: EX_out = in1 / in2;
+            `INC: EX_out = in1 + 1;
+            `DEC: EX_out = in1 - 1;
+            `AND: EX_out = in1 & in2;
+            `OR: EX_out = in1 | in2;
+            `XOR: EX_out = in1 ^ in2;
+            `ENCRY: EX_out = in1 ^ 32'b00110010101110101100100000011001; //XOR Encryption 
+            `DECRY: EX_out = in1 ^ 32'b00110010101110101100100000011001;
+            default: EX_out = 32'd0;
         endcase
     end
 
