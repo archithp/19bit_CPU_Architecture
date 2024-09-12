453476a3b13679ab51aebcd0fa2ed62d6e850f98
archithp <archith100@gmail.com>
2024-09-12 19:51:46 +0530

    variable name change

--- control.v
+++ control.v
@@ -2,34 +2,34 @@
 
 module control( input [4:0] opcode,
                 input hazard,
-                output reg branch,
-                output reg regwrite,
-                output reg memtoreg,
-                output reg memread,
-                output reg memwrite,
-                output reg alusrc,
-                output reg aluop,
-                output reg regdist,
-                output reg branchtype,
-                output reg push,
-                output reg pop,
-                output reg ret,
-                output reg jump);
+                output reg ID_branch,
+                output reg ID_regwrite,
+                output reg ID_memtoreg,
+                output reg ID_memread,
+                output reg ID_memwrite,
+                output reg ID_alusrc,
+                output reg ID_aluop,
+                output reg ID_regdist,
+                output reg ID_branchtype,
+                output reg ID_push,
+                output reg ID_pop,
+                output reg ID_ret,
+                output reg ID_jump);
 
     always @(*)begin
-        branch = 1'b0;
-        regwrite = 1'b0;
-        memtoreg = 1'b0;
-        memread = 1'b0;
-        memwrite = 1'b0;
-        alusrc = 1'b0;
-        aluop = 1'b0;
-        regdist = 1'b0;
-        branchtype = 1'b0;
-        push = 1'b0;
-        pop = 1'b0;
-        ret = 1'b0;
-        jump = 1'b0;
+        ID_branch = 1'b0;
+        ID_regwrite = 1'b0;
+        ID_memtoreg = 1'b0;
+        ID_memread = 1'b0;
+        ID_memwrite = 1'b0;
+        ID_alusrc = 1'b0;
+        ID_aluop = 1'b0;
+        ID_regdist = 1'b0;
+        ID_branchtype = 1'b0;
+        ID_push = 1'b0;
+        ID_pop = 1'b0;
+        ID_ret = 1'b0;
+        ID_jump = 1'b0;
 
         if (hazard) begin
             
@@ -37,57 +37,57 @@ module control( input [4:0] opcode,
         else begin
             case (opcode)
                 `ART: begin
-                    regdist = 1'b1;
-                    regwrite = 1'b1;
+                    ID_regdist = 1'b1;
+                    ID_regwrite = 1'b1;
                 end 
 
                 `LOG: begin
-                    regdist = 1'b1;
-                    regwrite = 1'b1;
+                    ID_regdist = 1'b1;
+                    ID_regwrite = 1'b1;
                 end 
             
                 `JMP: begin
-                    jump = 1'b1;
+                    ID_jump = 1'b1;
                 end
                 
                 `BQE: begin
-                    branch = 1'b1;
-                    branchtype = 1'b0;
+                    ID_branch = 1'b1;
+                    ID_branchtype = 1'b0;
                 end 
 
                 `BNE: begin
-                    branch = 1'b1;
-                    branchtype = 1'b1;
+                    ID_branch = 1'b1;
+                    ID_branchtype = 1'b1;
                 end 
 
                 `CALL: begin
-                    push = 1'b1;
-                    jump = 1'b1;
+                    ID_push = 1'b1;
+                    ID_jump = 1'b1;
                 end
                 
                 `RET: begin
-                    pop = 1'b1;
-                    ret = 1'b1;
+                    ID_pop = 1'b1;
+                    ID_ret = 1'b1;
                 end
 
                 `LD: begin
-                    memread = 1'b1;
-                    memtoreg = 1'b1;
-                    regwrite = 1'b1;
+                    ID_memread = 1'b1;
+                    ID_memtoreg = 1'b1;
+                    ID_regwrite = 1'b1;
                 end
 
                 `ST: begin
-                    memwrite = 1'b1;
+                    ID_memwrite = 1'b1;
                    end
 
                 `CRY: begin
-                    regwrite = 1'b1;
+                    ID_regwrite = 1'b1;
                 end          
             
                 `IMM: begin
-                    regdist = 1'b1;
-                    regwrite = 1'b1;
-                    alusrc = 1'b1;
+                    ID_regdist = 1'b1;
+                    ID_regwrite = 1'b1;
+                    ID_alusrc = 1'b1;
                 end               
                 
                 default: begin
