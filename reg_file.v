2939a0f72769bd7fb0f38b4db5d7464139703d3f
archithp <archith100@gmail.com>
2024-09-12 20:25:46 +0530

    rename ports, minor errot correction

--- reg_file.v
+++ reg_file.v
@@ -1,15 +1,15 @@
 module reg_file(input clk,
                 input reset,
-                input regwrite,
-                input push,
-                input pop,
+                input WB_regwrite,
+                input ID_push,
+                input ID_pop,
                 input [7:0]stack_pc,
                 input [2:0]rs1,
                 input [2:0]rs2,
                 input [2:0]ws,
                 input [31:0]wd,
-                output reg [31:0]rd1,
-                output reg [31:0]rd2);
+                output reg [31:0]ID_rd1,
+                output reg [31:0]ID_rd2);
     integer i;
     integer SP = 7;
 
@@ -18,33 +18,33 @@ module reg_file(input clk,
     //remove later
     initial begin
         register_file[0] = 32'd10;
-        register_file[1] = 32'd11;
-        register_file[2] = 32'd22;
-        register_file[3] = 32'd23;
+        register_file[1] = 32'd20;
+        register_file[2] = 32'd30;
+        register_file[3] = 32'd40;
     end
 
 
     always @(posedge clk or posedge reset) begin
-        rd1 = register_file[rs1];
-        rd2 = register_file[rs2];
+        ID_rd1 = register_file[rs1];
+        ID_rd2 = register_file[rs2];
 
         if (reset) begin
             for (i = 0; i < 32; i ++ ) begin
                 register_file[i] = 32'd0;
                 end
         end
-        else if (regwrite) begin
+        else if (WB_regwrite) begin
             register_file[ws] = wd;
             end
 
-        else if (push) begin
+        else if (ID_push) begin
             register_file[SP] = stack_pc + 1;
             SP = SP - 1;
         end
 
-        else if (pop) begin
+        else if (ID_pop) begin
             SP = SP + 1;
-            rd1 = register_file[SP];
+            ID_rd1 = register_file[SP];
         end
 
     end
@@ -54,7 +54,7 @@ module reg_file(input clk,
         integer i;
         begin
             $display("Register Values:");
-            for ( i = 0; i < 9 ; i ++ ) begin
+            for ( i = 0; i < 8 ; i ++ ) begin
                 $display("Register %d = %d",i,register_file[i]);
             end
         end
