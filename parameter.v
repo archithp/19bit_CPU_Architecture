91d43a4e54e77aff624bf101b86b9eddfb9c9faa
archithp <archith100@gmail.com>
2024-09-12 11:31:34 +0530

    combined ENC and DEC opcode to one opdoce CRY, Changed funct ENC to ENCRY

--- parameter.v
+++ parameter.v
@@ -7,9 +7,8 @@
 `define RET 5'd6
 `define LD 5'd7
 `define ST 5'd8
-`define ENC 5'd9
-`define DEC 5'd10
-`define IMM 5'd11
+`define CRY 5'd9
+`define IMM 5'd10
 
 `define ADD 5'd0
 `define SUB 5'd1
@@ -20,5 +19,5 @@
 `define AND 5'd6
 `define OR 5'd7
 `define XOR 5'd8
-`define ENC 5'd9
+`define ENCRY 5'd9
 `define DECRY 5'd10
