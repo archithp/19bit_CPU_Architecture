08a529243f89c858d297fded0cf23e93ffdc8656
archithp <archith100@gmail.com>
2024-09-14 17:56:49 +0530

    added IF_IDflush, branch miscalculation

--- IF_ID.v
+++ IF_ID.v
@@ -1,15 +1,22 @@
 module IF_ID (  input clk,
                 input [18:0] IF_instruction,
                 input IF_IDwrite,
+                input IF_IDflush,
                 input [7:0] IF_pc_plus_one,
                 output reg [7:0] ID_pc_plus_one,
                 output reg [18:0] ID_instruction);
 
     always @(posedge clk) begin
-        if (IF_IDwrite) begin
+        if (IF_IDflush) begin
+            ID_instruction <= 19'b1111111111111111111;
+            ID_pc_plus_one <= IF_pc_plus_one;
+        end
+
+        else if (IF_IDwrite) begin
             ID_instruction <= IF_instruction;
             ID_pc_plus_one <= IF_pc_plus_one;
         end
+        
     end
 
 endmodule
