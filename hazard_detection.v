0086228ab195901bd9b7b86d2430b9960793ed40
archithp <archith100@gmail.com>
2024-09-14 17:56:07 +0530

    added clk

--- hazard_detection.v
+++ hazard_detection.v
@@ -1,4 +1,5 @@
 module hazard_detection (
+    input clk,
     input EX_memread,
     input [2:0] EX_rt,
     input [2:0] ID_rt,
@@ -8,15 +9,16 @@ module hazard_detection (
     output reg PCwrite
 );
 
-    always @(*) begin
-        hazard = 1'b0;
-        IF_IDwrite = 1'b1;
-        PCwrite = 1'b1;
-
+    always @(posedge clk) begin
         if (EX_memread && ((EX_rt == ID_rt) || (EX_rt == ID_rs))) begin
             hazard = 1'b1;
             IF_IDwrite = 1'b0;
             PCwrite = 1'b0;
+        end
+        else begin
+            hazard = 1'b0;
+            IF_IDwrite = 1'b1;
+            PCwrite = 1'b1;        
         end
 
     end
