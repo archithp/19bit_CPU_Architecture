cc0a6b9f0a8b02bb65b02ac1a0714bb9c3e1b001
archithp <archith100@gmail.com>
2024-09-15 23:11:31 +0530

    added reset condition and few debug condition

--- hazard_detection.v
+++ hazard_detection.v
@@ -1,5 +1,5 @@
 module hazard_detection (
-    input clk,
+    input reset,
     input EX_memread,
     input [2:0] EX_rt,
     input [2:0] ID_rt,
@@ -9,12 +9,25 @@ module hazard_detection (
     output reg PCwrite
 );
 
-    always @(posedge clk) begin
-        if (EX_memread && ((EX_rt == ID_rt) || (EX_rt == ID_rs))) begin
+    always @(*) begin
+
+        if (reset) begin
+            hazard = 1'b0;
+            IF_IDwrite = 1'b1;
+            PCwrite = 1'b1;  
+        end
+
+        else if (EX_memread && ((EX_rt == ID_rt) || (EX_rt == ID_rs))) begin
             hazard = 1'b1;
             IF_IDwrite = 1'b0;
             PCwrite = 1'b0;
+            // $display("hazard");
+            // $display("EX_memread: %b", EX_memread);
+            // $display("ID_rs: %b", ID_rs);
+            // $display("ID_rt: %b", ID_rt);
+            // $display("EX_rt: %b", EX_rt);
         end
+
         else begin
             hazard = 1'b0;
             IF_IDwrite = 1'b1;
